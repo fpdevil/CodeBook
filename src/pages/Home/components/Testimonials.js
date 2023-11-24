97b664e197b85444d22607398c5e11f166a4e3ae
Sampath Singamsetty <sampathsingamsetty@Sampaths-MacBook-Pro.local>
2023-11-24 16:34:29 +0530

    Changed heading

--- src/pages/Home/components/Testimonials.js
+++ src/pages/Home/components/Testimonials.js
@@ -4,7 +4,7 @@ export const Testimonials = () => {
     return (
         <section className="my-4 rounded-md border shadow-sm dark:bg-gray-800 dark:border-slate-600">
             <h1 className="py-8 text-2xl font-medium text-center underline underline-offset-8 text-slate-500 dark:text-slate-100">
-                Testimonials About CodeBook
+                Users Voice
             </h1>
             <TestimonialCard />
         </section>
